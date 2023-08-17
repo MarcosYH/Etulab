3e1ddf89aac8ef7ae37e68e5dcbc878970abe8de
MarcosYH <marcosdeimos18@gmail.com>
2023-08-17 15:54:07 +0200

    initialise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-       <h1 className=' text-blue-900 font-bold text-6xl'> start</h1>
+    <div className="">
+     <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-        Navbar
-    </div>
+    <div>Navbar</div>
   )
 }
