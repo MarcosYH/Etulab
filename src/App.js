157c60c2c17f58d65f86e74a79e2753a4cbc9999
MarcosYH <marcosdeimos18@gmail.com>
2023-08-17 22:21:26 +0200

    feat: add router for page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Navbar from "./components/Navbar";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/App.css";
+import Accueil from "./pages/Accueil";
+import Solution from "./pages/Solution";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 function App() {
   return (
-    <div className="">
-     <Navbar/>
+    <div className="App">
+      {/* <Navbar /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Accueil/>} />
+          <Route path="/solution" element={<Solution/>} />
+         <Route path="/blog" element={<Blog/>} />
+         <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
