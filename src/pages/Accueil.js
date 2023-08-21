363520d50d9bc8ee679a673abc8f969a810e4579
MarcosYH <marcosdeimos18@gmail.com>
2023-08-21 16:11:00 +0200

    done page

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Accueil/Header";
+import About from "../components/Accueil/About";
+import Service from "../components/Accueil/Service";
+import LabTechnologies from "../components/Accueil/LabTechnologies";
+import Footer from "../components/Footer";
 
 export default function Accueil() {
   return (
     <div>
       <Navbar />
       <Header/>
+      <About/>
+      <Service/>
+      <LabTechnologies/>
+      <Footer/>
     </div>
   );
 }
