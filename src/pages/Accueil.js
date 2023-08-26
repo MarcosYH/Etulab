aaa1f3d30bee802cb259bb41394b1ed7a3ba09ba
MarcosYH <marcosdeimos18@gmail.com>
2023-08-26 23:36:43 +0200

    test

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,34 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Accueil/Header";
 import About from "../components/Accueil/About";
 import Service from "../components/Accueil/Service";
 import LabTechnologies from "../components/Accueil/LabTechnologies";
 import Footer from "../components/Footer";
-import Loader from "../components/Loader";
 
 export default function Accueil() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
   return (
     <div>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div>
-          <Navbar />
-          <Header/>
-          <About/>
-          <Service/>
-          <LabTechnologies/>
-          <Footer/>
-        </div>
-      )}
+      <Navbar />
+      <Header/>
+      <About/>
+      <Service/>
+      <LabTechnologies/>
+      <Footer/>
     </div>
   );
 }
