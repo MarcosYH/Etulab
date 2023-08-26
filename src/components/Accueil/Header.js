584aa673c5b9d8ffe5a1d0d4327f0d2eb955666b
MarcosYH <marcosdeimos18@gmail.com>
2023-08-26 21:38:50 +0200

    update header

--- src/components/Accueil/Header.js
+++ src/components/Accueil/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../styles/styles.css";
-import SecureAnimation from "./SecureAnimation";
+import SecureAnimation from "../SecureAnimation"
 
 export default function Header() {
   return (
@@ -8,7 +8,7 @@ export default function Header() {
     <div className="">
       <div className="flex flex-wrap items-center justify-between p-4 sm:mx-auto max-w-screen-xl mt-24 ">
         <div className="h-[30rem] ">
-          <div className=" md:w-1/2 p-4 title_header relative z-10 ">
+          <div className=" md:w-1/2 p-4 mb-5 title_header relative z-10 ">
             <div className=" mb-4">
             <span className=" p-2 bg-white shadow-lg border rounded-xl">👋 Bienvenue à Etulab</span>
             </div>
@@ -17,11 +17,11 @@ export default function Header() {
               de test !
             </h1>
             <div className=" border bg-[#34A853] rounded-xl py-1 w-16 mb-4"></div>
-            <p className="text-gray-600 text-2xl mr-16">Apprendre, appliquer et tester vos connaissances en sécurité.</p>
+            <p className="text-gray-600 text-2xl mr-16 mb-5">Apprendre, appliquer et tester vos connaissances en sécurité.</p>
             <a
                 href=" #"
                 type="button"
-                className="text-white rounded-lg px-3 py-2 text-center mr-3 md:mr-0 bg-[#2546BE] hover:bg-blue-900 my-5"
+                className="text-white rounded-lg px-3 py-2 text-center mr-3 md:mr-0 bg-[#2546BE] hover:bg-blue-900"
               >
                Commencer
               </a>
